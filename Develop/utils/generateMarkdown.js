96d644669c029e4144890bb03fb1f9474f61e1b1
Daniel S <dmschopler@gmail.com>
2023-05-18 14:32:38 -0400

    cleaned up code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 const renderLicenseBadge = (license) => {
   if (license !== 'Unlicensed') {
     return `[License](https://img.shields.io/static/v1?lavel=LICENSE&message=${license}&color=brightgreen)`
@@ -9,8 +7,6 @@ const renderLicenseBadge = (license) => {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 const renderLicenseLink = (license) => {
   if (license !== 'Unlicensed') {
     return `[License Page](https://opensource.org/license/${license.toLowerCase()})`
@@ -20,19 +16,10 @@ const renderLicenseLink = (license) => {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 const renderLicenseSection = (license) => {
   if (license !== 'Unlicensed') {
     return `## License Distributed under the ${license} License. See ${renderLicenseLink(license)} for more information.`
   }
 }
-
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
-
-// `;
-// }
 
 module.exports = generateMarkdown;
